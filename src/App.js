a657d1d424836c85f414e7e4de7f48606c77a333
Coleman Gardner <cole0769@gmail.com>
2021-03-21 10:06:27 -0700

    shop page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom'; // used for page routing.
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
 import './App.css';
 
-
 function App() {
+  // course section 76 4:20
   return (
     <div >
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} /> 
+        <Route exact path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-
+import { withRouter } from 'react-router-dom'; // For page history / navigation. avoids drilling through other components.
 import '../menu-item/menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
     <div style={{
         backgroundImage: `url(${imageUrl})`
     }}
-    className={`${size} menu-item`}
+    className={`${size} menu-item`} 
+    onClick={() => history.push( `${match.url}${linkUrl}`)}
     >
         <div 
             className='background-image' 
@@ -20,4 +21,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
     </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
