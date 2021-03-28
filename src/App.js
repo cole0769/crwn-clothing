2667448b9a8761062326d8c16d23cdc580930556
Coleman Gardner <cole0769@gmail.com>
2021-03-28 07:23:46 -0700

    header styles

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom'; // used for page routing.
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 import './App.css';
 
 function App() {
-  // course section 76 4:20
+  // course section 76 4:20 & header course section 82 5:12
   return (
     <div >
+      <Header /> 
       <Switch>
         <Route exact path='/' component={HomePage} /> 
         <Route exact path='/shop' component={ShopPage} />
