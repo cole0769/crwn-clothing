227c5da96e18f18e5846214954726eb68b54f974
Coleman Gardner <cole0769@gmail.com>
2021-03-28 19:44:49 -0700

    buttons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'; // used for page routing.
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component.jsx';
-import Header from './components/header/header.component.jsx';
 import './App.css';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
+import Header from './components/header/header.component';
+
+
 
 function App() {
   // course section 76 4:20 & header course section 82 5:12
@@ -13,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} /> 
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signin' component={SignInSignUpPage} />
       </Switch>
     </div>
   );
